3e36747fe9cc497e42c916a1770e9e148b97255a
Brune <abrune.osteo@gmail.com>
2023-05-03 22:21:05 +0200

    update and add interfaces

---
+++ src/utils.ts
@@ -0,0 +1,40 @@
+import { Permissions, LoyaltyUser } from "./enums"
+import { Review } from "./interfaces"
+
+const reviewTotalDisplay = document.querySelector('#reviews')
+const returningUserDisplay = document.querySelector('#returning-user')
+const userNameDisplay = document.querySelector('#user')
+
+
+export function showReviewTotal(value: number, reviewer: string, isLoyalty: LoyaltyUser) {
+    const iconDisplay = LoyaltyUser.GOLD_USER ? '⭐' : ''
+    reviewTotalDisplay.innerHTML = value.toString() + ' review' + makeMultiple(value) + ' | last reviewed by ' + reviewer + ' ' + iconDisplay    
+}
+
+export function populateUser(isReturning : boolean, userName: string ) {
+    if (isReturning == true){
+        returningUserDisplay.innerHTML = 'back'
+    }
+    userNameDisplay.innerHTML = userName
+}
+
+export function showDetails(value: boolean | Permissions, element : HTMLDivElement, price: number) {
+    if (value) {
+        const priceDisplay = document.createElement('div')
+        priceDisplay.innerHTML = price.toString() + '/night'
+        element.appendChild(priceDisplay)
+    }
+}
+
+export function makeMultiple(value: number) : string {
+    if (value > 1) {
+        return 's'
+    } else return ''
+}
+
+// Broken code
+export function getTopTwoReviews(reviews : Review[]) : Review[] {
+ const sortedReviews = reviews.sort((a, b) => b.stars - a.stars)
+ return sortedReviews.slice(0,2)
+}
+
